6eb9bb92716b163dd774c1b0984506641b7fee87
Jesus Hernandez <jesushdz2002ch14@gmail.com>
2025-07-03 11:47:47 -0400

    app deep clean, added search bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// This file renders root app, and connects it to the structure of the HTML file.
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
